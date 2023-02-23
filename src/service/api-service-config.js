f3a834aac1da6eb615f661964a3354db4ae73243
COSN\charlie <charlie@connective.com.au>
2023-02-23 16:54:39 +1100

    Update cors configure

--- src/service/api-service-config.js
+++ src/service/api-service-config.js
@@ -1,23 +1,20 @@
 import axios from "axios";
 
-const DEV_HOST = 'https://127.0.0.1:8089'
-const PROD_HOST = 'https://3.26.217.107:8089'
+// const DOMAIN = 'https://localhost:8089'
+const DOMAIN = 'https://3.26.217.107:8089'
 
 const DEFAULT_HEADERS = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS'
 }
 
 export const courseScoreApiClient = axios.create({
-    baseURL: `${PROD_HOST}/api/v1/course`,
+    baseURL: `${DOMAIN}/api/v1/course`,
     headers: DEFAULT_HEADERS,
 })
 
 export const stablefordScoreApiClient = axios.create({
-    baseURL: `${PROD_HOST}/api/v1/stableford`,
+    baseURL: `${DOMAIN}/api/v1/stableford`,
     headers: DEFAULT_HEADERS,
 })
 
